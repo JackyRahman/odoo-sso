d6f22f234a2bcf52584d175e91c9df20bf221166
jackyrahman14 <jackyrahman14@gmail.com>
2025-09-02 12:54:24 +0700

    feat: odoo sso

--- odoo_sso/__manifest__.py
+++ odoo_sso/__manifest__.py
@@ -5,8 +5,10 @@
     'category': 'Tools',
     'author': 'Custom',
     'depends': ['web'],
-    'data': ['views/templates.xml',
-             'views/res_config_settings_view.xml'],
+    'data': [
+        'views/templates.xml',
+        'views/res_config_settings_view.xml'
+    ],
     'assets': {
         'web.assets_backend': [
             '/odoo_sso/static/src/js/sso.js',
