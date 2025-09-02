ccf00e763f20cb6bf0d3f679fbb11331a0511a74
jackyrahman14 <jackyrahman14@gmail.com>
2025-09-02 12:04:55 +0700

    feat: odoo sso

--- odoo_sso/__init__.py
+++ odoo_sso/__init__.py
@@ -1,17 +1,2 @@
-{
-    'name': 'Odoo SSO',
-    'version': '16.0.1.0.0',
-    'summary': 'Basic SSO login/logout with /me endpoint',
-    'category': 'Tools',
-    'author': 'Custom',
-    'depends': ['web'],
-    'data': ['views/templates.xml',
-             'views/res_config_settings_view.xml'],
-    'assets': {
-        'web.assets_backend': [
-            '/odoo_sso/static/src/js/sso.js',
-        ],
-    },
-    'installable': True,
-    'application': False,
-}
+from . import controllers
+from . import models
